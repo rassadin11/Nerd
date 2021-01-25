be101d3ef421f6d9060eacaee54bb0d8ec8ce91c
rassadin11 <58170052+rassadin11@users.noreply.github.com>
2021-01-25 20:14:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,298 @@
+// Dynamic Adapt v.1
+// HTML data-da="where(uniq class name),position(digi),when(breakpoint)"
+// e.x. data-da="item,2,992"
+// Andrikanych Yevhen 2020
+// https://www.youtube.com/c/freelancerlifestyle
+
+"use strict";
+
+(function () {
+	let originalPositions = [];
+	let daElements = document.querySelectorAll('[data-da]');
+	let daElementsArray = [];
+	let daMatchMedia = [];
+	//Заполняем массивы
+	if (daElements.length > 0) {
+		let number = 0;
+		for (let index = 0; index < daElements.length; index++) {
+			const daElement = daElements[index];
+			const daMove = daElement.getAttribute('data-da');
+			if (daMove != '') {
+				const daArray = daMove.split(',');
+				const daPlace = daArray[1] ? daArray[1].trim() : 'last';
+				const daBreakpoint = daArray[2] ? daArray[2].trim() : '767';
+				const daType = daArray[3] === 'min' ? daArray[3].trim() : 'max';
+				const daDestination = document.querySelector('.' + daArray[0].trim())
+				if (daArray.length > 0 && daDestination) {
+					daElement.setAttribute('data-da-index', number);
+					//Заполняем массив первоначальных позиций
+					originalPositions[number] = {
+						"parent": daElement.parentNode,
+						"index": indexInParent(daElement)
+					};
+					//Заполняем массив элементов 
+					daElementsArray[number] = {
+						"element": daElement,
+						"destination": document.querySelector('.' + daArray[0].trim()),
+						"place": daPlace,
+						"breakpoint": daBreakpoint,
+						"type": daType
+					}
+					number++;
+				}
+			}
+		}
+		dynamicAdaptSort(daElementsArray);
+
+		//Создаем события в точке брейкпоинта
+		for (let index = 0; index < daElementsArray.length; index++) {
+			const el = daElementsArray[index];
+			const daBreakpoint = el.breakpoint;
+			const daType = el.type;
+
+			daMatchMedia.push(window.matchMedia("(" + daType + "-width: " + daBreakpoint + "px)"));
+			daMatchMedia[index].addListener(dynamicAdapt);
+		}
+	}
+	//Основная функция
+	function dynamicAdapt(e) {
+		for (let index = 0; index < daElementsArray.length; index++) {
+			const el = daElementsArray[index];
+			const daElement = el.element;
+			const daDestination = el.destination;
+			const daPlace = el.place;
+			const daBreakpoint = el.breakpoint;
+			const daClassname = "_dynamic_adapt_" + daBreakpoint;
+
+			if (daMatchMedia[index].matches) {
+				//Перебрасываем элементы
+				if (!daElement.classList.contains(daClassname)) {
+					let actualIndex = indexOfElements(daDestination)[daPlace];
+					if (daPlace === 'first') {
+						actualIndex = indexOfElements(daDestination)[0];
+					} else if (daPlace === 'last') {
+						actualIndex = indexOfElements(daDestination)[indexOfElements(daDestination).length];
+					}
+					daDestination.insertBefore(daElement, daDestination.children[actualIndex]);
+					daElement.classList.add(daClassname);
+				}
+			} else {
+				//Возвращаем на место
+				if (daElement.classList.contains(daClassname)) {
+					dynamicAdaptBack(daElement);
+					daElement.classList.remove(daClassname);
+				}
+			}
+		}
+		customAdapt();
+	}
+
+	//Вызов основной функции
+	dynamicAdapt();
+
+	//Функция возврата на место
+	function dynamicAdaptBack(el) {
+		const daIndex = el.getAttribute('data-da-index');
+		const originalPlace = originalPositions[daIndex];
+		const parentPlace = originalPlace['parent'];
+		const indexPlace = originalPlace['index'];
+		const actualIndex = indexOfElements(parentPlace, true)[indexPlace];
+		parentPlace.insertBefore(el, parentPlace.children[actualIndex]);
+	}
+	//Функция получения индекса внутри родителя
+	function indexInParent(el) {
+		var children = Array.prototype.slice.call(el.parentNode.children);
+		return children.indexOf(el);
+	}
+	//Функция получения массива индексов элементов внутри родителя 
+	function indexOfElements(parent, back) {
+		const children = parent.children;
+		const childrenArray = [];
+		for (let i = 0; i < children.length; i++) {
+			const childrenElement = children[i];
+			if (back) {
+				childrenArray.push(i);
+			} else {
+				//Исключая перенесенный элемент
+				if (childrenElement.getAttribute('data-da') == null) {
+					childrenArray.push(i);
+				}
+			}
+		}
+		return childrenArray;
+	}
+	//Сортировка объекта
+	function dynamicAdaptSort(arr) {
+		arr.sort(function (a, b) {
+			if (a.breakpoint > b.breakpoint) { return -1 } else { return 1 }
+		});
+		arr.sort(function (a, b) {
+			if (a.place > b.place) { return 1 } else { return -1 }
+		});
+	}
+	//Дополнительные сценарии адаптации
+	function customAdapt() {
+		//const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	}
+}());;
+let swiper = new Swiper('.image-slider', {
+	pagination: {
+		el: '.swiper-pagination',
+    	type: 'bullets',
+    	clickable: true,
+	},
+})
+
+let pagination = document.querySelector('.swiper-pagination-bullet-active');
+let elems = document.querySelectorAll('.swiper-pagination-bullet');
+let elem = document.createElement('span');
+
+if (elems.length > 1) {
+	for (let elem of elems) {
+		if (elem.firstChild) {
+			elem.firstChild.remove()
+		}
+	}
+}
+
+elem.classList.add('swiper-pagination-temporary')
+pagination.append(elem)
+
+swiper.on('slideChange', () => {
+	pagination = document.querySelector('.swiper-pagination-bullet-active');
+	elems = document.querySelectorAll('.swiper-pagination-bullet');
+	elem = document.createElement('span');
+
+	if (elems.length > 1) {
+		for (let elem of elems) {
+			if (elem.firstChild) {
+				elem.firstChild.remove()
+			}
+		}
+	}
+	elem.classList.add('swiper-pagination-temporary')
+	pagination.append(elem)
+})
+
+swiper.init();;
+const burger = document.querySelector('.icon-menu');
+
+burger.addEventListener('click', () => {
+	const menuList = document.querySelector('.menu__list');
+	
+	document.body.classList.toggle('hidden')
+	menuList.classList.toggle('active');
+	burger.classList.toggle('active');
+});
+let animationTime = 1000; // ms
+let numb = 150;
+let step = 1;
+let value = animationTime / numb / step;
+let i = 0;
+let number = 0;
+
+function animation(num, elem) {
+	let timeout = setTimeout(() => {
+		if (num >= number) {
+			if (i > numb / step - 15) { // замедление "15" нужно менять
+				value += 10;
+			}
+			i++;
+
+			elem.firstChild.innerHTML = number; // изменение числа
+			number += step;
+			animation(num, elem);
+		} else if (num < number) {
+			clearTimeout(timeout)
+		}
+	}, value)
+};
+
+let itemAdvantages = document.querySelectorAll('.item-advantages');
+let j = 0;
+
+for (let elem of itemAdvantages) {
+	if (elem.clientHeight > j) {
+		j = elem.clientHeight;
+	}
+}
+
+for (let elem of itemAdvantages) {
+	elem.style.minHeight = j + 'px';
+}
+
+let itemDesign = document.querySelectorAll('.item-design-right1');
+let m = 0;
+
+for (let elem of itemDesign) {
+	if (elem.clientHeight > m) {
+		m = elem.clientHeight;
+	}
+}
+
+for (let elem of itemDesign) {
+	elem.style.minHeight = m + 'px';
+}
+
+if (document.body.clientWidth < 768 && document.body.clientWidth > 600) {
+	console.log(itemAdvantages.length)
+	if (itemAdvantages % 2 != 0) {
+		itemAdvantages[itemAdvantages.length - 1].classList.add('resize');
+		console.log(itemAdvantages)
+	}
+}
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/myIcon.gif',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        }),
+
+        myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+            hintContent: 'Собственный значок метки с контентом',
+            balloonContent: 'А эта — новогодняя',
+            iconContent: '12'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: '../img/first/map-marker.png',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
+});
